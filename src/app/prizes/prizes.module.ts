3cb21a1ff7b146f365a7aa7e2d436dba3cade264
developingstar <petr.nocolae812@gmail.com>
2018-04-03 16:46:17 +0200

    Refactore Index Table, Fix CSS Style

--- src/app/prizes/prizes.module.ts
+++ src/app/prizes/prizes.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
 import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
 import { LoadingModule } from 'ngx-loading';
+import { TooltipModule } from 'ngx-bootstrap';
 
 import * as Services from '../app.services-list';
 import { routing } from '../app.routing';
@@ -36,6 +37,7 @@ import { AwardedPrizesIndexComponent } from './awardedprizesindex/awardedprizesi
     SharedModule,
     ModalModule.forRoot(),
     ButtonsModule.forRoot(),
+    TooltipModule.forRoot(),
     BsDatepickerModule.forRoot()
   ],
   providers: [
