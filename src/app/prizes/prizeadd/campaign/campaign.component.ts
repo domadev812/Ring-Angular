2abcd231530a0488594a418732270ba104221eac
developingstar <petr.nocolae812@gmail.com>
2018-04-17 07:15:31 +0200

    add delete campaign

--- src/app/prizes/prizeadd/campaign/campaign.component.ts
+++ src/app/prizes/prizeadd/campaign/campaign.component.ts
@@ -2,7 +2,7 @@ import 'rxjs/add/observable/throw';
 import * as moment from 'moment';
 import { Component, OnInit, TemplateRef, Input, Output, EventEmitter } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 import { error } from 'util';
@@ -16,7 +16,7 @@ import { Campaign } from '../../../_models/campaign.model';
   styleUrls: ['./campaign.component.scss']
 })
 
-export class CampaignComponent implements OnInit {    
+export class CampaignComponent implements OnInit {
   @Input() prizeId;
   @Input() campaigns;
   @Output() refreshPage = new EventEmitter();
@@ -32,25 +32,25 @@ export class CampaignComponent implements OnInit {
   campaignId: number;
   startDate: Date = new Date();
   endDate: Date = new Date();
-  minDate: Date = new Date();  
+  minDate: Date = new Date();
   quantity: string;
   redeemed: number;
   released: number;
   selectedIndex: number;
   today: any;
 
-  constructor(private prizesService: PrizesService, 
-              private modalService: BsModalService) { }
+  constructor(private prizesService: PrizesService,
+    private modalService: BsModalService) { }
 
-  ngOnInit() {      
+  ngOnInit() {
     this.modalType = 0;
-    this.modalTitle = 'New Campaign';   
+    this.modalTitle = 'New Campaign';
     this.today = moment().format('YYYY-MM-DD');
-  } 
+  }
 
   openModal(template: TemplateRef<any>, index: number = -1): void {
     this.modalRef = this.modalService.show(template, this.config);
-    this.campaignId = index === -1 ? 0 : this.campaigns[index].id;        
+    this.campaignId = index === -1 ? 0 : this.campaigns[index].id;
     this.selectedIndex = index;
     if (this.campaignId === 0) {
       this.modalTitle = 'New Campaign';
@@ -62,10 +62,10 @@ export class CampaignComponent implements OnInit {
       this.modalTitle = 'Edit Campaign';
       this.startDate = new Date(this.campaigns[index].activation_start);
       this.endDate = new Date(this.campaigns[index].activation_end);
-      this.quantity = this.campaigns[index].number_available;      
+      this.quantity = this.campaigns[index].number_available;
       this.redeemed = this.campaigns[index].redeemed;
       this.released = this.campaigns[index].released;
-    }   
+    }
   }
 
   tooltip(redeemed: boolean): string {
@@ -78,35 +78,35 @@ export class CampaignComponent implements OnInit {
 
   formatDate(date: any): string {
     return date ? moment(date, moment.ISO_8601)
-      .format('DD  MMM  YYYY') : '';    
+      .format('DD  MMM  YYYY') : '';
   }
-  
+
   checkValue(): boolean {
     return (parseInt(this.quantity, 0) >= this.redeemed) ? true : false;
   }
 
-  saveCampaign(valid): void {    
+  saveCampaign(valid): void {
     if (!valid) {
       return;
     }
     if (!this.startDate || !this.endDate) {
       return;
     }
-    
-    let newCampaign = {      
+
+    let newCampaign = {
       activation_start: this.startDate,
       activation_end: this.endDate,
       number_available: this.quantity,
-      release_dates: [{activation_start: this.startDate, activation_end: this.endDate}]            
+      release_dates: [{ activation_start: this.startDate, activation_end: this.endDate }]
     };
-    
-    if (this.campaignId === 0) {      
-      this.prizesService.createCampaign(this.prizeId, new Model.Campaign(newCampaign)).subscribe( (res) => {                 
-        alert('Campaign is created');          
+
+    if (this.campaignId === 0) {
+      this.prizesService.createCampaign(this.prizeId, new Model.Campaign(newCampaign)).subscribe((res) => {
+        alert('Campaign is created');
         this.campaigns.push(res);
         this.modalRef.hide();
         this.refreshPage.emit();
-      }, (errors) => {              
+      }, (errors) => {
         alert(errors.message);
       });
     } else {
@@ -114,12 +114,12 @@ export class CampaignComponent implements OnInit {
         return;
       }
 
-      this.prizesService.updateCampaign(this.prizeId, this.campaignId, this.quantity).subscribe( (res) => {                 
-        alert('Campaign is updated');           
+      this.prizesService.updateCampaign(this.prizeId, this.campaignId, this.quantity).subscribe((res) => {
+        alert('Campaign is updated');
         this.campaigns.splice(this.selectedIndex, 1, res);
         this.modalRef.hide();
         this.refreshPage.emit();
-      }, (errors) => {              
+      }, (errors) => {
         alert(errors.message);
       });
     }
@@ -141,20 +141,19 @@ export class CampaignComponent implements OnInit {
     if (!this.startDate || !this.endDate) {
       return;
     }
-    if (moment(value).isAfter(this.endDate)) {      
+    if (moment(value).isAfter(this.endDate)) {
       this.endDate = moment(value).toDate();
       this.endDate.setDate(this.endDate.getDate() + 1);
-    }        
+    }
   }
 
-  deleteCampaign(redeemed: number, campaignId: number, prizeId: string): void {
-    if (!redeemed) {
-      this.prizesService.deleteCampaign(prizeId, campaignId).subscribe( (res) => {                 
-        alert('Campaign is deleted');
-        this.refreshPage.emit();
-      }, (errors) => {              
-        alert(errors.message);
-      });
-    }
+  deleteCampaign(campaignId: number, prizeId: string): void {
+    this.prizesService.deleteCampaign(this.prizeId, this.campaignId).subscribe((res) => {
+      alert('Campaign is deleted');
+      this.refreshPage.emit();
+      this.modalRef.hide();
+    }, (errors) => {
+      alert(errors.message);
+    });
   }
 }
