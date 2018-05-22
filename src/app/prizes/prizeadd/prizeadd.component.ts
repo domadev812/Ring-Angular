84917fafaee47c9e5bb594db7553a2b101b56383
developingstar <petr.nocolae812@gmail.com>
2018-05-22 16:48:53 +0200

    fix prize create default orgId

--- src/app/prizes/prizeadd/prizeadd.component.ts
+++ src/app/prizes/prizeadd/prizeadd.component.ts
@@ -4,11 +4,12 @@ import { ActivatedRoute, Routes, RouterModule, Router } from '@angular/router';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { error } from 'util';
-import { MultiSelectService, PrizesService, CurrentUserService, AuthService } from '../../app.services-list';
+import { MultiSelectService, PrizesService, CurrentUserService, AuthService, GroupService } from '../../app.services-list';
 import { Model } from '../../app.models-list';
 import { GlobalState } from '../../global.state';
 import { MultiSelectUtil } from '../../_utils/multiselect.util';
 import { NavbarService } from '../../app.services-list';
+import { Prize } from '../../_models/prize.model';
 
 @Component({
   selector: 'app-prizeadd',
@@ -18,7 +19,7 @@ import { NavbarService } from '../../app.services-list';
 export class PrizeAddComponent implements OnInit {
   prize: Model.Prize;
   originalPrize: Model.Prize;
-  title: string;
+  pageTitle: string;
   public editFlag: boolean;
   public disableFlag: boolean;
   public sponsorList = [];
@@ -26,9 +27,17 @@ export class PrizeAddComponent implements OnInit {
   public deliveryList = [{ itemName: 'In-House', id: 1 }, { itemName: 'Third Party', id: 2 }];
   public selectedDelivery = [];
   public ktsSelectSettings: any = {};
+  public selectAllMultiSettings: any = {};
   public ktsDeliverySelectSettings = {};
   public creating = false;
   public points: number;
+  public groupList = [];
+  public selectedGroups = [];
+  public limit: number;
+  public offset: number;
+  public loading = false;
+  public group_ids: Array<Model.Group>;
+  public groups: Array<Model.Group>;
 
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -37,26 +46,32 @@ export class PrizeAddComponent implements OnInit {
     public global: GlobalState,
     public navBarService: NavbarService,
     private currentUserService: CurrentUserService,
-    public authProvider: AuthService
+    public authProvider: AuthService,
+    private groupService: GroupService,
   ) { }
 
   ngOnInit() {
     this.navBarService.show();
     this.navBarService.activeTabChanged('prizes');
-    this.title = 'New Prize';
+    this.getSponsors();
+    this.pageTitle = 'New Prize';
     this.prize = new Model.Prize({});
+    this.originalPrize = new Model.Prize({});
+    this.offset = 0;
+    this.limit = 50;
     this.editFlag = false;
     this.disableFlag = false;
+    this.selectAllMultiSettings = MultiSelectUtil.selectAllMultiSettings;
     this.ktsDeliverySelectSettings = MultiSelectUtil.singleDeliverySelection;
     const id = this.route.snapshot.paramMap.get('prizeId');
-    if (id !== null) {
-      this.title = 'Edit Prize';
+    this.getGroups();
+    this.getUser();
+    if (id) {
+      this.pageTitle = 'Edit Prize';
       this.editFlag = true;
       this.disableFlag = true;
       this.getPrize(id);
     }
-    this.getSponsors();
-    this.getUser();
   }
 
   getUser() {
@@ -65,9 +80,31 @@ export class PrizeAddComponent implements OnInit {
         this.setAdminStatus(res.roles);
         const org = res.organization;
         if (!this.editFlag) {
-          this.selectedSponsor.push(new MultiSelectUtil.SelectItem(org.name, this.prize.id));
+          this.selectedSponsor.push(new MultiSelectUtil.SelectItem(org.name, res.organization_id));
         }
       }
+    }, err => {
+      console.log('err', err);
+    });
+  }
+
+  getSponsors(): void {
+    this.multiSelectService.getDropdownSponsors().subscribe((res: MultiSelectUtil.SelectItem[]) => {
+      this.sponsorList = res;
+      if (this.editFlag && this.prize.organization_id && this.sponsorList.length > 0) {
+        let org = this.sponsorList.find(sponsor => sponsor.id === parseInt(this.prize.organization_id, 0));
+        if (org) {
+          this.selectedSponsor.push(org);
+        }
+      }
+    }, err => {
+      console.log('err', err);
+    });
+  }
+
+  getGroups(): void {
+    this.multiSelectService.getDropdownGroups().subscribe((res: MultiSelectUtil.SelectItem[]) => {
+      this.groupList = res;
     }, err => {
       console.log('err', err);
     });
@@ -99,25 +136,38 @@ export class PrizeAddComponent implements OnInit {
     this.onChange(item);
   }
 
+  onGroupSelect(item: any) {
+    this.onChange(item);
+  }
+
+  onGroupDeSelect(item: any) {
+    this.onChange(item);
+  }
+
   goBack(): void {
     this.router.navigate(['prizes']);
+  }
+
+  isGroupsChanged(): boolean {
+    return this.selectedGroups.length > 0 ? false : true;
+  }
+
+  prizeSuccess(prize: Prize) {
+    this.creating = false;
+    this.prize = prize;
+    this.originalPrize = Object.assign({}, prize);
+    this.selectedSponsor.push(new MultiSelectUtil.SelectItem(this.prize.organization.name, this.prize.organization_id));
+    this.selectedGroups = this.prize.groups.map(group => new MultiSelectUtil.SelectItem(group.name, group.id));
+    let delivery = this.deliveryList.find(type => type.itemName.toLowerCase() === this.prize.delivery_type.toLowerCase());
+    if (delivery) {
+      this.selectedDelivery.push(delivery);
+    }
   }
 
   getPrize(id, flag = true): void {
     this.creating = true;
     this.prizesService.getPrize(id).subscribe((res) => {
-      this.prize = res;
-      this.originalPrize = Object.assign({}, this.prize);
-      if (this.sponsorList.length > 0) {
-        let org = this.sponsorList.find(sponsor => sponsor.id === parseInt(this.prize.organization_id, 0));
-        if (org) {
-          this.selectedSponsor.push(org);
-        }
-      }
-      let delivery = this.deliveryList.find(deliveryType => deliveryType.itemName === this.prize.delivery_type);
-      if (delivery) {
-        this.selectedDelivery.push(delivery);
-      }
+      this.prizeSuccess(res);
       this.creating = false;
       this.disableFlag = flag;
     }, (errors) => {
@@ -140,11 +190,23 @@ export class PrizeAddComponent implements OnInit {
         this.disableFlag = false;
         return;
       }
+      if (this.selectedGroups.length === 0) {
+        this.disableFlag = false;
+        return;
+      } else if (this.selectedGroups !== this.originalPrize.group_ids) {
+        this.disableFlag = false;
+        return;
+      }
 
       if (this.selectedDelivery.length === 0) {
         this.disableFlag = false;
         return;
       } else if (this.selectedDelivery[0].itemName !== this.originalPrize.delivery_type) {
+        this.disableFlag = false;
+        return;
+      }
+
+      if (!this.isGroupsChanged()) {
         this.disableFlag = false;
         return;
       }
@@ -164,44 +226,31 @@ export class PrizeAddComponent implements OnInit {
       return;
     }
 
+    if (this.selectedGroups.length === 0 || this.selectedGroups.length === 0) {
+      return;
+    }
+
+    this.prize.group_ids = this.selectedGroups.map(group => {
+      return group.id;
+    });
+
     this.prize.organization_id = this.selectedSponsor[0].id;
-    this.creating = true;
     if (!this.prize.id) {
       this.prizesService.createPrize(this.prize).subscribe((res) => {
-        this.creating = false;
         alert('Prize is created');
         this.global.selectedTab = 'prizes';
         this.router.navigate(['prizes']);
       }, (errors) => {
-        this.creating = false;
         alert('Server error');
       });
     } else {
-      this.creating = false;
-      alert('Prize is updated');
-      this.global.selectedTab = 'prizes';
-      this.router.navigate(['prizes']);
-      // this.prizesService.updatePrize(this.prize).subscribe( (res) => {      
-      //   alert('Prize is updated');  
-      //   this.global.selectedTab = 'prizes';
-      //   this.router.navigate(['prizes']);       
-      // }, (errors) => {      
-      //   alert('Server error');
-      // });
+      this.prizesService.updatePrize(this.prize).subscribe((res) => {
+        alert('Prize is updated');
+        this.global.selectedTab = 'prizes';
+        this.router.navigate(['prizes']);
+      }, (errors) => {
+        alert('Server error');
+      });
     }
-  }
-
-  getSponsors(): void {
-    this.multiSelectService.getDropdownSponsors().subscribe((res: MultiSelectUtil.SelectItem[]) => {
-      this.sponsorList = res;
-      if (this.editFlag && this.prize.organization_id && this.sponsorList.length > 0) {
-        let org = this.sponsorList.find(sponsor => sponsor.id === parseInt(this.prize.organization_id, 0));
-        if (org) {
-          this.selectedSponsor.push(org);
-        }
-      }
-    }, err => {
-      console.log('err', err);
-    });
   }
 }
